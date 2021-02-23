83cfd317e5bf2132ea833b0cfdcf8cc10b244503
Jesse Clark <hello@developerjesse.com>
2021-02-23 15:20:55 +0200

    Add rLogin for testing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss'
-import rLogin from '@rsksmart/rlogin';
+import { rLogin } from './rLogin'
 
 function App () {
+  const [response, setResponse] = useState<any>(null)
+  interface logItem {
+    id: number
+    text: string,
+  }
+
+  const [log, setLog] = useState<logItem[]>([{ id: 0, text: 'Startup' }])
+
+  const logIt = (text: string) => setLog([...log, { text, id: log.length }])
+
+  const handleConnect = () => {
+    logIt('connecting...')
+    rLogin.connect()
+      .then((response: any) => {
+        logIt('Connected!')
+        setResponse(response)
+      })
+      .catch((err: Error) => logIt(err.toString()))
+  }
+
+  const handleDisconnect = () => {
+    response.disconnect()
+    setResponse(null)
+    logIt('Disconnect.')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Testing rLogin Integrated!
       </header>
 
+      <div className="interaction">
+        <h2>Start here!</h2>
+        <button onClick={handleConnect}>Connect ;-)</button>
+        <button onClick={() => localStorage.clear()}>Clear Local Storage</button>
+
+        {response && (
+          <>
+            <button onClick={handleDisconnect}>Disconnect</button>
+            <p>Address: </p>
+            <p>ChainId: </p>
+          </>
+        )}
+      </div>
+
+      <div className="log">
+        <h2>Log:</h2>
+        <ol>
+          {log.map((item: logItem) => <li key={item.id}>{item.text}</li>)}
+        </ol>
+      </div>
       <footer>
         Published version: 1
       </footer>
---
+++ src/rLogin.ts
@@ -0,0 +1,18 @@
+import RLogin from '@rsksmart/rlogin'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+
+export const rLogin = new RLogin({
+  cacheProvider: false,
+  providerOptions: {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        rpc: {
+          30: 'https://did.rsk.co:4444',
+          31: 'https://did.testnet.rsk.co:4444'
+        }
+      }
+    }
+  },
+  supportedChains: [1, 3, 4, 5, 30, 31, 42, 5777]
+})
